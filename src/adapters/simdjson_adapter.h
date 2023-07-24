85ec49e8383c99ceb2720f1cb31a7298f01f314e
Max Norfolk <mnorfolk@psu.edu>
2023-07-24 12:42:38 -0400

    Updated SimdJsonAdapter to use the verison of simdjson. Added a basic test for the new adapter

--- src/adapters/simdjson_adapter.h
+++ src/adapters/simdjson_adapter.h
@@ -6,172 +6,263 @@
 #include <unordered_map>
 #include <cstdint>
 #include <cassert>
+#include <utility>
 #include <vector>
 
 #ifdef _MSC_VER
 #define NOMINMAX
 #endif
-#include <simdjson/parsedjson.h>
-#include <simdjson/jsonparser.h>
+
+#include <simdjson.h>
 #include "adapters/common_utils.h"
+
+constexpr size_t DEFAULT_BATCH_SIZE = 1 << 24;
 
 using namespace simdjson;
 
 namespace fishstore {
-namespace adapter {
+    namespace adapter {
 
-class SIMDJsonField {
-public:
-  SIMDJsonField(int64_t id_, const ParsedJson::Iterator& it_)
-    : field_id(id_), iter(it_) {}
+        // Represents a SimdJson field
+        class SIMDJsonField {
+        public:
+            // constructs a SimdJsonField with a given simdjson value
+            SIMDJsonField(int64_t id_, const simdjson_result <ondemand::value> &value_)
+                    : field_id(id_), simd_value(value_) {}
 
-  inline int64_t FieldId() const {
-    return field_id;
-  }
+            inline int64_t FieldId() const {
+                return field_id;
+            }
 
-  inline NullableBool GetAsBool() const {
-    switch (iter.get_type()) {
-    case 't':
-      return NullableBool(true);
-    case 'f':
-      return NullableBool(false);
-    default:
-      return NullableBool();
+            inline NullableBool GetAsBool() const {
+                bool val;
+                bool has_value = (simd_value.get(val) == error_code::SUCCESS);
+                return {has_value, val};
+            }
+
+            inline NullableInt GetAsInt() const {
+                int64_t val;
+                bool has_value = (simd_value.get(val) == error_code::SUCCESS);
+                return {has_value, static_cast<int32_t>(val)};
+            }
+
+            inline NullableLong GetAsLong() const {
+                int64_t val;
+                bool has_value = (simd_value.get(val) == error_code::SUCCESS);
+                return {has_value, val};
+            }
+
+            inline NullableFloat GetAsFloat() const {
+                double val;
+                bool has_value = (simd_value.get(val) == error_code::SUCCESS);
+                return {has_value, static_cast<float>(val)};
+            }
+
+            inline NullableDouble GetAsDouble() const {
+                double val;
+                bool has_value = (simd_value.get(val) == error_code::SUCCESS);
+                return {has_value, val};
+            }
+
+            inline NullableString GetAsString() const {
+
+                // the simdjson get method only supports std::string_view, so we must
+                // turn it into a std::string after
+                std::string_view temp{};
+                bool has_value = (simd_value.get(temp) == error_code::SUCCESS);
+                return {has_value, std::string(temp)};
+            }
+
+            inline NullableStringRef GetAsStringRef() const {
+                std::string_view temp{};
+                bool has_value = (simd_value.get(temp) == error_code::SUCCESS);
+                StringRef str_ref{temp.data(), temp.length()};
+                return {has_value, str_ref};
+            }
+
+        private:
+            int64_t field_id;
+            mutable simdjson_result <ondemand::value> simd_value;
+        };
+
+        // represents the type of field that will be looked up (object or array)
+        enum class SIMDJsonFieldType {
+            OBJECT,
+            ARRAY
+        };
+
+        // represents the type of field that will be looked up and
+        // the name or index that should be looked up
+        // this is stored in a vector in SIMDJsonFieldLookup
+        struct SIMDJsonFieldLookupElement {
+            SIMDJsonFieldType type;
+            union {
+                std::string_view key_name;
+                size_t array_index;
+            };
+        };
+
+        // helper class used to look up simdjson fields
+        class SIMDJsonFieldLookup {
+        public:
+            // creates a simdjson field from a string
+            SIMDJsonFieldLookup() = default;
+
+            explicit SIMDJsonFieldLookup(const std::string &lookup_str_) : lookup_str(
+                    std::make_unique<std::string>(lookup_str_)) {
+                char *start = lookup_str->data();
+                char *end = lookup_str->data();
+                while (true) {
+                    if (*end == '.' || *end == '\0') { // end of  a field
+                        size_t str_len = end - start;
+                        if (str_len == 0) {
+                            break;
+                        }
+                        std::string_view field = {start, str_len};
+                        SIMDJsonFieldLookupElement item = {.type=SIMDJsonFieldType::OBJECT, .key_name=field};
+                        lookups.push_back(item);
+
+                        if (*end == '\0') {
+                            break;
+                        }
+
+
+                        start = end + 1;
+                    } else if (*start == '[') {
+                        // strtol will move the end pointer right after the last number, so to the ']'.
+                        size_t index = std::strtol(++start, &end, 10);
+                        SIMDJsonFieldLookupElement item = {.type=SIMDJsonFieldType::ARRAY, .array_index=index};
+                        lookups.push_back(item);
+                        start = end + 1;
+                    }
+                    end++;
+                }
+            }
+
+            simdjson_result <ondemand::value> find(ondemand::object source) const {
+                auto fields_it = lookups.begin();
+
+                // always starts with an object
+                auto ret = source.find_field_unordered(fields_it->key_name);
+                fields_it++;
+
+                // iterate through the entire lookups
+                while (fields_it != lookups.end()) {
+                    // if object, or array find the correct value
+                    if (fields_it->type == SIMDJsonFieldType::OBJECT) {
+                        ret = ret.find_field_unordered(fields_it->key_name);
+                    } else if (fields_it->type == SIMDJsonFieldType::ARRAY) {
+                        ret = ret.at(fields_it->array_index);
+                    }
+                    fields_it++;
+                }
+
+                return ret;
+            }
+
+            std::string ToString() const {
+                return *lookup_str;
+            }
+
+        private:
+            std::vector<SIMDJsonFieldLookupElement> lookups;
+            std::unique_ptr<std::string> lookup_str; // kept around for memory management
+        };
+
+        class SIMDJsonRecord {
+        public:
+            friend class SIMDJsonParser;
+
+            SIMDJsonRecord() = default;
+
+            SIMDJsonRecord(ondemand::document_reference doc, const std::vector<SIMDJsonFieldLookup> &lookups) {
+                obj = doc.get_object();
+                int i = 0;
+                for (const auto &lookup: lookups) {
+                    const auto value = lookup.find(obj);
+                    // check the value was found
+                    if (value.error() == simdjson::SUCCESS) {
+                        fields.emplace_back(i, value);
+                    } else {
+                        fprintf(stderr, "FIELD NOT FOUND: [%s]\n", lookup.ToString().c_str());
+                    }
+                    ++i;
+                }
+            }
+
+            inline const std::vector<SIMDJsonField> &GetFields() const {
+                return fields;
+            }
+
+            inline StringRef GetRawText() const {
+                auto ref = obj.raw_json().value();
+                return {ref.data(), ref.length()};
+            }
+
+        public:
+            mutable ondemand::object obj;
+            std::vector<SIMDJsonField> fields;
+        };
+
+        class SIMDJsonParser {
+        public:
+            SIMDJsonParser(const std::vector<std::string> &field_names) {
+                field_lookups.reserve(field_names.size());
+                for (const auto &item: field_names) {
+                    field_lookups.emplace_back(item);
+                }
+            }
+
+            inline void Load(const char *buffer, size_t length) {
+                if (parser.iterate_many(buffer, length, DEFAULT_BATCH_SIZE).get(docs) != simdjson::SUCCESS)
+                    return;
+                docs_it = docs.begin();
+            }
+
+            inline bool HasNext() {
+                return docs_it != docs.end();
+            }
+
+            inline const SIMDJsonRecord &NextRecord() {
+                record = SIMDJsonRecord(*docs_it, field_lookups);
+                ++docs_it;
+                return record;
+            }
+
+        private:
+            std::vector<SIMDJsonFieldLookup> field_lookups;
+
+            // keep these around for memory safety reasons
+            ondemand::parser parser;
+            ondemand::document_stream docs;
+            ondemand::document_stream::iterator docs_it;
+
+            SIMDJsonRecord record;
+        };
+
+        class SIMDJsonAdapter {
+        public:
+            typedef SIMDJsonParser parser_t;
+            typedef SIMDJsonField field_t;
+            typedef SIMDJsonRecord record_t;
+
+            inline static parser_t *NewParser(const std::vector<std::string> &fields) {
+                return new parser_t{fields};
+            }
+
+            inline static void Load(parser_t *const parser, const char *payload, size_t length, size_t offset = 0) {
+                assert(offset <= length);
+                parser->Load(payload + offset, length - offset);
+            }
+
+            inline static bool HasNext(parser_t *const parser) {
+                return parser->HasNext();
+            }
+
+            inline static const record_t &NextRecord(parser_t *const parser) {
+                return parser->NextRecord();
+            }
+        };
     }
-  }
-
-  inline NullableInt GetAsInt() const {
-    if (iter.is_integer()) {
-      return NullableInt(static_cast<int32_t>(iter.get_integer()));
-    } else return NullableInt();
-  }
-
-  inline NullableLong GetAsLong() const {
-    if (iter.is_integer()) {
-      return NullableLong(iter.get_integer());
-    } else return NullableLong();
-  }
-
-  inline NullableFloat GetAsFloat() const {
-    if (iter.is_double()) {
-      return NullableFloat(static_cast<float>(iter.get_double()));
-    } else return NullableFloat();
-  }
-
-  inline NullableDouble GetAsDouble() const {
-    if (iter.is_double()) {
-      return NullableDouble(iter.get_double());
-    } else return NullableDouble();
-  }
-
-  inline NullableString GetAsString() const {
-    if (iter.is_string()) {
-      return NullableString(std::string(iter.get_string(), iter.get_string_length()));
-    } else return NullableString();
-  }
-
-  inline NullableStringRef GetAsStringRef() const {
-    if (iter.is_string()) {
-      return NullableStringRef(StringRef(iter.get_string(), iter.get_string_length()));
-    } else return NullableStringRef();
-  }
-
-private:
-  int64_t field_id;
-  ParsedJson::Iterator iter;
-};
-
-class SIMDJsonRecord {
-public:
-  friend class SIMDJsonParser;
-
-  SIMDJsonRecord() : original(), fields() {}
-
-  SIMDJsonRecord(const char* data, size_t length)
-    : original(data, length) {
-    fields.clear();
-  }
-
-  inline const std::vector<SIMDJsonField>& GetFields() const {
-    return fields;
-  }
-
-  inline StringRef GetRawText() const {
-    return original;
-  }
-
-public:
-  StringRef original;
-  std::vector<SIMDJsonField> fields;
-};
-
-class SIMDJsonParser {
-public:
-  SIMDJsonParser(const std::vector<std::string>& field_names, const size_t alloc_bytes = 1LL << 25)
-  : fields(field_names) {
-    auto success = pj.allocate_capacity(alloc_bytes);
-    assert(success);
-    has_next = false;
-  }
-
-  inline void Load(const char* buffer, size_t length) {
-    record.original = StringRef(buffer, length);
-    record.fields.clear();
-    auto ok = json_parse(buffer, length, pj);
-    if (ok != 0 || !pj.is_valid()) {
-      printf("Parsing failed...\n");
-      has_next = false;
-    } else {
-      has_next = true;
-    }
-  }
-
-  inline bool HasNext() {
-    return has_next;
-  }
-
-  inline const SIMDJsonRecord& NextRecord() {
-    ParsedJson::Iterator it(pj);
-    for (auto field_id = 0; field_id < fields.size(); ++field_id) {
-      if (it.move_to(fields[field_id])) {
-        record.fields.emplace_back(SIMDJsonField{field_id, it});
-      }
-    }
-    has_next = false;
-    return record;
-  }
-
-private:
-  std::vector<std::string> fields;
-  ParsedJson pj;
-  SIMDJsonRecord record;
-  bool has_next;
-};
-
-class SIMDJsonAdapter {
-public:
-  typedef SIMDJsonParser parser_t;
-  typedef SIMDJsonField field_t;
-  typedef SIMDJsonRecord record_t;
-
-  inline static parser_t* NewParser(const std::vector<std::string>& fields) {
-    return new parser_t{ fields };
-  }
-
-  inline static void Load(parser_t* const parser, const char* payload, size_t length, size_t offset = 0) {
-    assert(offset <= length);
-    parser->Load(payload + offset, length - offset);
-  }
-
-  inline static bool HasNext(parser_t* const parser) {
-    return parser->HasNext();
-  }
-
-  inline static const record_t& NextRecord(parser_t* const parser) {
-    return parser->NextRecord();
-  }
-};
-
-}
 }
 
---
+++ test/simdjson_adapter_test.cc
@@ -0,0 +1,34 @@
+// A basic testing framework for simdjson seperate from FishStore
+//
+// Created by Max Norfolk on 7/23/23.
+
+#include "gtest/gtest.h"
+#include "adapters/simdjson_adapter.h"
+
+using adapter_t = fishstore::adapter::SIMDJsonAdapter;
+using parser_t = fishstore::adapter::SIMDJsonParser;
+
+std::string raw_json = R"XX(
+{"id":3, "school":{"id":6}, "random":"garbage13"}
+{"id":6, "school":{"id":7}, "random":"garbage24"}
+{"id":100, "school":{"id":3}, "random":"garbage35"}
+)XX";
+
+TEST(SimdJsonTests, BasicTest1) {
+    parser_t parser({"id", "school.id"});
+    parser.Load(raw_json.data(), raw_json.length());
+    ASSERT_TRUE(parser.HasNext());
+    auto rec = parser.NextRecord();
+    auto &fields = rec.GetFields();
+    auto raw_text = rec.GetRawText();
+    std::string str = {raw_text.Data(), rec.GetRawText().Length()};
+    printf("Raw String: %s\n", str.data());
+    ASSERT_EQ(fields.size(), 2);
+    ASSERT_TRUE(fields[0].GetAsInt().HasValue());
+}
+
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
